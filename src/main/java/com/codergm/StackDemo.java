0b6dd959b485a71fe581bc7c63fa100d86886047
Idris Askarov <id.askarov@gmail.com>
2023-11-23 11:52:55 -0800

    Queue demo

---
+++ src/main/java/com/codergm/StackDemo.java
@@ -0,0 +1,23 @@
+package com.codergm;
+
+import com.codergm.keydatestructures.TemperatureRecord;
+
+import java.time.LocalDateTime;
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack<TemperatureRecord> stack = new Stack();
+        TemperatureRecord t1 = new TemperatureRecord(23.5, "NY", LocalDateTime.now());
+        TemperatureRecord t2 = new TemperatureRecord(13.5, "Tokyo", LocalDateTime.now());
+        TemperatureRecord t3 = new TemperatureRecord(-3.8, "London ", LocalDateTime.now());
+        stack.push(t1);
+        stack.push(t2);
+        stack.push(t3);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
